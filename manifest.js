64931fed8f6e57583eb263ecadcad21c41a45b34
zulramsey7 <zulfikarazizul7@gmail.com>
2025-08-04 22:05:15 +0800

    public

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Iqra' Galaksi",
+  "short_name": "IqraGalaksi",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0d1b2a",
+  "theme_color": "#2563eb",
+  "icons": [
+    {
+      "src": "/icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
