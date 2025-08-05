009c9ed6b4dd9349a374fa5747bc204fc06c3073
zulramsey7 <zulfikarazizul7@gmail.com>
2025-08-05 08:29:28 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
+const CACHE_NAME = 'iqra-galaksi-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/output.css',
+  '/audio/lagu-latar.mp3',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/manifest.json',
+  // Tambah fail penting lain di sini
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('iqra-cache').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/input.css',
-        '/output.css',
-        '/audio/alif.mp3', // tambah fail penting
-        // ...
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
